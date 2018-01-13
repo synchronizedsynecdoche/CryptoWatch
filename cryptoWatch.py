76dbc1c544d8f6941445f723897e3dbbfcc90cac
patrick <kelly237@purdue.edu>
2018-01-13 13:57:10 -0500

    grab the whole thing, really inefficient

--- cryptoWatch.py
+++ cryptoWatch.py
@@ -7,31 +7,30 @@ p.add_argument("longform", metavar='name:amount', type=str, nargs='+', help="Exp
 arr = p.parse_args()
 
 
-
 def query(crypto, fiat):
 
     # give the option of grabbing straight from this link for less than 10 coins
-    #url = "https://api.coinmarketcap.com/v1/ticker/{}/?convert={}".format(crypto, fiat)
+    # url = "https://api.coinmarketcap.com/v1/ticker/{}/?convert={}".format(crypto, fiat)
     url = "https://api.coinmarketcap.com/v1/ticker/?convert=USD&limit=0"
     data = urllib.urlopen(url)
     readable = json.load(data)
 
-    iter = 0
+    iterator = 0
     while True:
         try:
-            if(readable[iter]['id'] == crypto or readable[iter]['name'] == crypto or readable[iter]['symbol'] == crypto.upper()):
-                return readable[iter]['price_{}'.format(fiat)]
-            iter += 1
+            if readable[iterator]['id'] == crypto or readable[iterator]['name'] == crypto or readable[iterator]['symbol'] == crypto.upper():
+                return readable[iterator]['price_{}'.format(fiat)]
+            iterator += 1
+
         except IndexError:
             raise Exception("Invalid Cryptocurrency")
 
-    return 1
 
-iter = 0
+iterator = 0
 while True:
 
     try:
-        print(query(getattr(arr,'longform')[iter].split(":")[0],'usd'))
-        iter += 1
+        print(query(getattr(arr,'longform')[iterator].split(":")[0],'usd'))
+        iterator += 1
     except IndexError:
         break
