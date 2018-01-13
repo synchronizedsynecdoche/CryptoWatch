c808b58cd0e2031b4a109682b7a40cea45c4bfe5
patrick <kelly237@purdue.edu>
2018-01-13 12:45:39 -0500

    query(crypto, fiat)

--- cryptoWatch.py
+++ cryptoWatch.py
@@ -1,10 +1,18 @@
 import argparse
+import json
+import urllib
 
 p = argparse.ArgumentParser(description="Small program for fetching crypto values")
-p.add_argument("longform", metavar='Name', type=str, nargs='+', help="Expects the longform name of a cryptocurrency (\"Bitcoin\", not \"BTC\")")
-a = p.parse_args()
+p.add_argument("longform", metavar='name:amount', type=str, nargs='+', help="Expects the longform name of a cryptocurrency (\"Bitcoin\", not \"BTC\") followed by the amount you own")
+arr = p.parse_args()
 
-def query(arr):
 
-    #for i in range(0,len(arr)):
+def query(crypto, fiat):
+
+    url = "https://api.coinmarketcap.com/v1/ticker/{}/?convert={}".format(crypto, fiat)
+    data = json.load(urllib.urlopen(url))
+    print(data)
+
+query("bitcoin", "usd")
+
 
