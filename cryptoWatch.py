33668e798d733df29d53f930ff4308013e9729dc
patrick <kelly237@purdue.edu>
2018-01-13 10:50:31 -0500

    started playing with argparse, work on logic tomorrow

--- cryptoWatch.py
+++ cryptoWatch.py
@@ -1,6 +1,10 @@
 import argparse
 
 p = argparse.ArgumentParser(description="Small program for fetching crypto values")
-p.add_argument("longform", metavar='name', type=str, nargs='+', help="Expects the longform name of a cryptocurrency, (\"Bitcoin\", not \"BTC\"")
+p.add_argument("longform", metavar='Name', type=str, nargs='+', help="Expects the longform name of a cryptocurrency (\"Bitcoin\", not \"BTC\")")
 a = p.parse_args()
-print(a.longform[0])
+
+def query(arr):
+
+    #for i in range(0,len(arr)):
+
