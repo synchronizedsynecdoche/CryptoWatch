49c218f12ab5791bab5f642ce97d5113d1187ed5
patrick <kelly237@purdue.edu>
2018-01-19 16:42:00 -0500

    further fixed ambiguous error handling

--- cryptoWatch.py
+++ cryptoWatch.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
+
 import argparse
 import json
 import urllib
+from datetime import datetime
 
 p = argparse.ArgumentParser(description="Small program for fetching crypto values")
 
-p.add_argument('-f', '--fiat',metavar='fiat',type=str,default='usd', help="expects the shorthand ticker symbol for a fiat"
-                                                                 "supported by coinmarketcap")
+p.add_argument('-f', '--fiat',metavar='fiat',type=str,default='usd', help="expects the shorthand ticker symbol for "
+                                                                          "a fiat supported by coinmarketcap")
 
 p.add_argument("longform", metavar='longform', type=str, nargs='+', help="Expects the name or symbol of a "
                "cryptocurrency (\"Bitcoin\" or  \"BTC\") followed by the amount you own")
@@ -14,43 +16,66 @@ p.add_argument("longform", metavar='longform', type=str, nargs='+', help="Expect
 arr = p.parse_args()
 
 
-def query(crypto, fiat):
+def makeAPICall(fiat):
 
-    # give the option of grabbing straight from this link for less than 10 coins
-    # url = "https://api.coinmarketcap.com/v1/ticker/{}/?convert={}".format(crypto, fiat)
     url = "https://api.coinmarketcap.com/v1/ticker/?convert={}&limit=0".format(fiat)
     data = urllib.urlopen(url)
     readable = json.load(data)
 
+    return readable
+
+
+def query(crypto, fiat):
+
+    readable = makeAPICall(fiat)
     iterator = 0
+
     while True:
         try:
+            try:
+                x = readable[iterator]['price_{}'.format(fiat)]
+
+            except KeyError:
+                raise Exception("Invalid fiat")
+
             if readable[iterator]['id'] == crypto or readable[iterator]['name'] == crypto or readable[iterator]['symbol'] == crypto.upper():
+
                 return float(readable[iterator]['price_{}'.format(fiat)])
+
             iterator += 1
 
         except IndexError or KeyError:
-            raise Exception("Invalid cryptocurrency or fiat currency")
+            raise Exception("Invalid cryptocurrency")
 
 
-iterator = 0
+time = datetime.fromtimestamp(int(makeAPICall(arr.fiat)[0]['last_updated'])).strftime('%m/%d/%Y %H:%M:%S')  # in a rare situation (the site updates between this line and when the loop starts) this will be off by 5 minutes
+print("Values updated at {}\n--------------------".format(time))
+
+i = 0
 sigma = 0
 while True:
 
     try:
-        x = getattr(arr,'longform')[iterator].split(":")[1]
-    except IndexError:
-        try:
-            raise Exception("Invalid amount for {}".format(getattr(arr,'longform')[iterator].upper().split(":")[0]))
-        except: # end of the line
-            pass
-    try:
-	amount = query(arr.longform[iterator].upper().split(":")[0],arr.fiat.lower()) * float(arr.longform[iterator].upper().split(":")[1])
+        x = arr.longform[i].split(":")[1]
 
-        print(arr.longform[iterator].upper().split(":")[0] +" : "+str(amount))
-        iterator += 1
-	sigma += amount
     except IndexError:
-        print("Total : "+str(sigma))
-	break
-	
+
+        try:
+
+            raise Exception("Invalid amount for {}".format(arr.longform)[i].upper().split(":")[0])
+
+        except:  # end of the line
+            pass
+
+    try:
+
+        amount = query(arr.longform[i].upper().split(":")[0],arr.fiat.lower()) * float(arr.longform[i].upper().split(":")[1])
+        print(arr.longform[i].upper().split(":")[0] +" : "+str(amount))
+        i += 1
+        sigma += amount
+
+    except IndexError:
+
+        print("--------------------\nTotal : "+str(sigma))
+        break
+
